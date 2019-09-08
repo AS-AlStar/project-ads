191ccf4f1537f9e4b778b4b1ccbba290b0d42ef0
AS-AlStar <starovojtov.alexander@gmail.com>
2019-09-08 19:24:04 +0300

    Create pages for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,8 @@ class User < ApplicationRecord
   has_many :ads, dependent: :destroy
 
   validates :nickname, :email, presence: true
+
+  def full_name
+    [first_name, last_name].join(' ')
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,34 +1,38 @@
 # frozen_string_literal: true
 
-def print_user(user)
-  puts [Rainbow('Email').blue, Rainbow(user.email).green].join(' : ')
-  puts [Rainbow('Password').blue, Rainbow(user.password).green].join(' : ')
-  puts Rainbow('*' * 20).yellow
+Rails.logger = Logger.new(STDOUT)
+
+def colorize_hash(hash)
+  hash.map { |k, v| [Rainbow(k.to_s.upcase).blue, Rainbow(v).green].join(' : ') }
 end
 
-admin, *others = 10.times.map do |index|
-  User.create!(
-      email: Faker::Internet.email,
-      password: Faker::Internet.password,
-      nickname: Faker::Twitter.screen_name,
-      role: index.zero? ? 'admin' : 'default',
-      first_name: Faker::Name.first_name,
-      last_name: Faker::Name.last_name
+def print_user(user)
+  strs = colorize_hash(email: user.email, password: user.password) + Array(Rainbow('*' * 20).yellow)
+  strs.each(&Rails.logger.method(:info))
+end
 
+admin, *others = Array.new(10).map.with_index do |_, index|
+  User.create!(
+    email: Faker::Internet.email,
+    password: Faker::Internet.password,
+    nickname: Faker::Twitter.screen_name,
+    role: index.zero? ? 'admin' : 'default',
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name
   )
 end
 
 others.each do |user|
-  user.ads = 10.times.map do
+  user.ads = Array.new(10).map do
     Ad.new(
-        title: Faker::Book.title,
-        body: Faker::Books::Lovecraft.paragraph_by_chars(characters: 1024),
-        state: Ad.state.values.sample
+      title: Faker::Book.title,
+      body: Faker::Books::Lovecraft.paragraph_by_chars(characters: 1024),
+      state: Ad.state.values.sample
     )
   end
 end
 
-puts 'Admin user'
+Rails.logger.info 'Admin user'
 print_user(admin)
-puts 'Other users'
+Rails.logger.info 'Other users'
 others.each(&method(:print_user))
