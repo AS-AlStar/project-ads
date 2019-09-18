f7c75edc560eb22c098a68b66ce3a4c249279149
AS-AlStar <starovojtov.alexander@gmail.com>
2019-09-18 14:35:48 +0300

    Add localisation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,28 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+  # around_action :switch_locale
+  #
+  # def switch_locale(&action)
+  #   locale = params[:locale] || I18n.default_locale
+  #   I18n.with_locale(locale, &action)
+  # end
+
   rescue_from CanCan::AccessDenied, with: :access_denied
 
   def access_denied(exception)
     store_location_for :user, request.path
     redirect_to user_signed_in? ? root_path : new_user_session_path, alert: exception.message
+  end
+
+  private
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options(options = {})
+    { locale: I18n.locale }.merge options
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'home/index'
-  devise_for :users, controllers: { registrations: 'users/registrations' }
-  ActiveAdmin.routes(self)
-  root to: 'home#index'
-  resources :ads
-  resources :categories do
+  scope '(:locale)', locale: /#{I18n.available_locales.join("|")}/ do
+    get 'home/index'
+    devise_for :users, controllers: { registrations: 'users/registrations' }
+    ActiveAdmin.routes(self)
+    root to: 'home#index'
     resources :ads
+    resources :categories, only: %i[show]
+    resources :users, only: %i[show]
   end
-  resources :users, only: %i[show]
 end
